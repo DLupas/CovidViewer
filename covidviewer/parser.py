fc59e552e074bffea4469ff897c18142c4b8612f
DLupas <lupas.danny@gmail.com>
2021-06-11 04:53:44 +0000

    parse page properly

--- covidviewer/parser.py
+++ covidviewer/parser.py
@@ -1,8 +1,27 @@
 from bs4 import BeautifulSoup
+from selenium import webdriver
+#selenium is requiered to render javascript 
+#The tables on the page are generated dynamically and requests will only get html
 import requests
 
 dataURL = "https://health-infobase.canada.ca/covid-19/epidemiological-summary-covid-19-cases.html"
-page = requests.get(dataURL)
+driver = webdriver.Firefox()
+driver.get(dataURL)
+page = driver.page_source
+#page = requests.get(dataURL)
 
-soup = BeautifulSoup(page.content, "lxml")
-print(soup.prettify())
+soup = BeautifulSoup(page, "lxml")
+#print(soup.prettify())
+
+bodyContainer = soup.find(class_="nationalNumbers-data")
+for trTag in bodyContainer.find_all("tr"):
+    #print(trTag)
+    data = trTag.find_all("td")
+    print(data)
+
+'''
+#for testing purposes
+with open("page.txt", "w") as page:
+    page.write(soup.prettify())
+page.close()
+'''
