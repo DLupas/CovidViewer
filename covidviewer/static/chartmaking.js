81b8bfce5c2019dd1e0514001f100868556c3a3e
AnanthChebolu <achebolu4@gmail.com>
2021-06-14 23:38:28 -0400

    Added a chart for past data

---
+++ covidviewer/static/chartmaking.js
@@ -0,0 +1,48 @@
+var xmlHttp = new XMLHttpRequest();
+var url = "static/data.json";
+xmlHttp.open("GET", url, true);
+xmlHttp.send();
+xmlHttp.onreadystatechange = function() {
+    if(this.readyState == 4 && this.status == 200) {
+        var data = JSON.parse(this.responseText);
+        //console.log(data);
+        var date = data.covid.map(function(elem) {
+            return elem.date;
+        });
+        var cases = data.covid.map(function(elem) {
+            return elem.cases;
+        });
+        var deaths = data.covid.map(function(elem) {
+            return elem.deaths;
+        });
+        var ctx = document.getElementById('canvas').getContext('2d');
+        var myChart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: date,
+                datasets: [{
+                    label: '# of Cases',
+                    data: cases,
+                    backgroundColor: 'transparent',
+                    borderColor:'blue' ,
+                    borderWidth: 4
+                },
+                {
+                    label: '# of Deaths',
+                    data: deaths,
+                    backgroundColor: 'transparent',
+                    borderColor:'red' ,
+                    borderWidth: 4
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+        //console.log(dates);
+    }
+}
